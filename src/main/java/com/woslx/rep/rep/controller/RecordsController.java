b7d316f0f593617249c5bbb8c9a35ea8c7fb9fff
huangyue <hy@zizizizizi.com>
2016-10-16 16:44:11 +0800

    add query

--- src/main/java/com/woslx/rep/rep/controller/RecordsController.java
+++ src/main/java/com/woslx/rep/rep/controller/RecordsController.java
@@ -4,7 +4,10 @@ import com.woslx.rep.common.ApiException;
 import com.woslx.rep.common.ApiResult;
 import com.woslx.rep.common.BaseController;
 import com.woslx.rep.common.Constants;
-import com.woslx.rep.rep.entity.*;
+import com.woslx.rep.rep.entity.Item;
+import com.woslx.rep.rep.entity.ItemName;
+import com.woslx.rep.rep.entity.ItemType;
+import com.woslx.rep.rep.entity.Records;
 import com.woslx.rep.rep.entity.param.*;
 import com.woslx.rep.rep.entity.vo.Opertion;
 import com.woslx.rep.rep.entity.vo.OpertionMsg;
@@ -26,6 +29,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -177,21 +181,18 @@ public class RecordsController extends BaseController {
                 if (price != null) {
                     records.setPrice(price);
                     records.setPricePutIn(1);   //外部设定
-                }
-                else {
+                } else {
                     price = item.getPrice();
                     records.setPrice(price);
                     records.setPricePutIn(0);   //内设定
                 }
 
                 Integer priceAll = out.getPriceAll();
-                if(priceAll != null)
-                {
+                if (priceAll != null) {
                     records.setAllPrice(priceAll);
                     records.setAllPricePutIn(1);   //外部设定
-                }
-                else {
-                    records.setAllPrice((int)Math.round(price* out.getQuality()/10.0));
+                } else {
+                    records.setAllPrice((int) Math.round(price * out.getQuality() / 10.0));
                     records.setAllPricePutIn(0);   //内设定
                 }
             }
@@ -210,8 +211,7 @@ public class RecordsController extends BaseController {
 
             records.setQuantity(quality);
             item.setQuantityUse(item.getQuantityUse() + quality);       //修改出库数量
-            if(item.getQuantityAll() - item.getQuantityUse() < 0)
-            {
+            if (item.getQuantityAll() - item.getQuantityUse() < 0) {
                 throw new RuntimeException("出库数量大于库存");
             }
             item.setQuantityCurrent(item.getQuantityAll() - item.getQuantityUse());     //修改当前数量
@@ -287,77 +287,128 @@ public class RecordsController extends BaseController {
         ApiResult<Opertion> apiResult = new ApiResult<>(0, Constants.SUCCESS);
 
         Opertion opertion = new Opertion();
+        List<OpertionMsg> msgList = new ArrayList<>();
+        opertion.setList(msgList);
+        opertion.setSum(0);
+        apiResult.setData(opertion);
 
         Integer queryType = queryOpertionCondition.getQueryType();
-        if(queryType == 1)      //多条件
+        if (queryType == 1)      //多条件
         {
+            Calendar calendar = Calendar.getInstance();
             Date end = queryOpertionCondition.getEnd();
             Date start = queryOpertionCondition.getStart();
+            calendar.setTime(start);
+            calendar.set(Calendar.HOUR,0);
+            start = calendar.getTime();
+            calendar.setTime(end);
+            calendar.set(Calendar.HOUR,0);
+            calendar.add(Calendar.DATE,1);
+            end = calendar.getTime();
+
+            if (start == null || end == null) {
+                apiResult.setCode(1);
+                apiResult.setMessage("起止时间必须输入");
+                return apiResult.toString();
+            }
+
+            if(start.getTime() > end.getTime())
+            {
+                apiResult.setCode(1);
+                apiResult.setMessage("起始时间必须小于结束时间");
+                return apiResult.toString();
+            }
+
             List<String> gentaiList = queryOpertionCondition.getGentaiList();
             List<String> docNameList = queryOpertionCondition.getDocNameList();
 
             //1. 查询到所有在合适日期内的所有住院号,同时符合医生，跟台人
             List<String> zhuyuanNoList = recordsService.queryOperation(docNameList, gentaiList, start, end);
 
-
             //2. 根据住院号查询,查询一个，保存一个
-
-        }
-        else    //按住院号
-        {
-            String zhuyuanNO = queryOpertionCondition.getZhuyuanNO();
-            List<Records> recordsList = recordsService.getRecordsByZhuyuanNo(zhuyuanNO);
-
-            if (recordsList.size() == 0) {
+            if (zhuyuanNoList.size() == 0) {
                 apiResult.setCode(1);
                 apiResult.setMessage("未查找到记录");
                 return apiResult.toString();
-            }
-            else {
-                Records records = recordsList.get(0);
-                List<OpertionMsg> msgList = new ArrayList<>();
-                opertion.setList(msgList);
+            } else {
+                for (String zhuyuanno : zhuyuanNoList) {
 
-                OpertionMsg opertionMsg = new OpertionMsg();
-                opertionMsg.setDocName(records.getDocterName());
-                opertionMsg.setGentai(records.getGentaiName());
-                opertionMsg.setZhuyuanNo(records.getZhuyuanNo());
-                opertionMsg.setPatientName(records.getPatientName());
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
-                opertionMsg.setDate(sdf.format(records.getTime()));
-                msgList.add(opertionMsg);
+                    OpertionMsg opertionMsg = getOpertionMsgByZhuyuanNo(zhuyuanno);
+                    msgList.add(opertionMsg);
+                    if (opertionMsg == null) {
+                        logger.error("住院号：{}未找到任何记录", zhuyuanno);
+                        apiResult.setCode(1);
+                        apiResult.setMessage("住院号："+zhuyuanno+"未找到任何记录");
+                        return apiResult.toString();
+                    } else {
 
-                List<RecordsVO> recordsVOList = new ArrayList<>();
-                opertionMsg.setRecordsVOList(recordsVOList);
-                Integer total = 0;
-                for (Records rtemp : recordsList) {
-                    RecordsVO vo = new RecordsVO();
-                    Integer itemTypeId = rtemp.getItemTypeId();
-                    Integer itemNameId = rtemp.getItemNameId();
-
-                    ItemName itemName = itemNameService.getById(itemNameId);
-                    ItemType itemType = itemTypeService.getById(itemTypeId);
-                    Item item = itemService.getById(rtemp.getItemId());
-                    vo.setItemType(itemType.getName());
-                    vo.setItemName(itemName.getName());
-                    vo.setQuality(rtemp.getQuantity()/10.0);
-                    vo.setPrice(rtemp.getPrice());
-                    vo.setPricePutIn(rtemp.getPricePutIn());
-                    vo.setAllPrice(rtemp.getAllPrice());
-                    vo.setAllPricePutIn(rtemp.getAllPricePutIn());
-                    vo.setItemSpec(item.getSpecifications());
-                    total = total+ rtemp.getAllPrice();
-
-                    recordsVOList.add(vo);
+                        opertion.setSum(opertion.getSum() + opertionMsg.getTotal());
+                    }
                 }
-                opertionMsg.setTotal(total);
+            }
+        } else    //按住院号
+        {
+            String zhuyuanNO = queryOpertionCondition.getZhuyuanNO();
 
-                apiResult.setData(opertion);
-
+            OpertionMsg opertionMsg = getOpertionMsgByZhuyuanNo(zhuyuanNO);
+            msgList.add(opertionMsg);
+            if (opertionMsg == null) {
+                apiResult.setCode(1);
+                apiResult.setMessage("未查找到记录");
+                return apiResult.toString();
+            } else {
+                opertion.setSum(opertionMsg.getTotal());
             }
         }
 
         return apiResult.toString();
+    }
+
+    private OpertionMsg getOpertionMsgByZhuyuanNo(String zhuyuanNo) {
+        List<Records> recordsList = recordsService.getRecordsByZhuyuanNo(zhuyuanNo);
+        OpertionMsg opertionMsg = null;
+        if (recordsList.size() == 0) {
+            return null;
+        } else {
+            Records records = recordsList.get(0);
+            List<OpertionMsg> msgList = new ArrayList<>();
+
+            opertionMsg = new OpertionMsg();
+            opertionMsg.setDocName(records.getDocterName());
+            opertionMsg.setGentai(records.getGentaiName());
+            opertionMsg.setZhuyuanNo(records.getZhuyuanNo());
+            opertionMsg.setPatientName(records.getPatientName());
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+            opertionMsg.setDate(sdf.format(records.getTime()));
+            msgList.add(opertionMsg);
+
+            List<RecordsVO> recordsVOList = new ArrayList<>();
+            opertionMsg.setRecordsVOList(recordsVOList);
+            Integer total = 0;
+            for (Records rtemp : recordsList) {
+                RecordsVO vo = new RecordsVO();
+                Integer itemTypeId = rtemp.getItemTypeId();
+                Integer itemNameId = rtemp.getItemNameId();
+
+                ItemName itemName = itemNameService.getById(itemNameId);
+                ItemType itemType = itemTypeService.getById(itemTypeId);
+                Item item = itemService.getById(rtemp.getItemId());
+                vo.setItemType(itemType.getName());
+                vo.setItemName(itemName.getName());
+                vo.setQuality(rtemp.getQuantity() / 10.0);
+                vo.setPrice(rtemp.getPrice());
+                vo.setPricePutIn(rtemp.getPricePutIn());
+                vo.setAllPrice(rtemp.getAllPrice());
+                vo.setAllPricePutIn(rtemp.getAllPricePutIn());
+                vo.setItemSpec(item.getSpecifications());
+                total = total + rtemp.getAllPrice();
+
+                recordsVOList.add(vo);
+            }
+            opertionMsg.setTotal(total);
+        }
+
+        return opertionMsg;
     }
 
 
@@ -401,9 +452,9 @@ public class RecordsController extends BaseController {
         recordsVOList.setItemName(itemName.getName());
         recordsVOList.setSn(sn);
         recordsVOList.setSpec(item.getSpecifications());
-        recordsVOList.setAllIn(item.getQuantityAll()/10.0);
-        recordsVOList.setAllOut(item.getQuantityUse()/10.0);
-        recordsVOList.setCur(item.getQuantityCurrent()/10.0);
+        recordsVOList.setAllIn(item.getQuantityAll() / 10.0);
+        recordsVOList.setAllOut(item.getQuantityUse() / 10.0);
+        recordsVOList.setCur(item.getQuantityCurrent() / 10.0);
         recordsVOList.setId(item.getId());
         List<RecordsVO> list2 = new ArrayList<>();
         recordsVOList.setList(list2);
@@ -411,16 +462,15 @@ public class RecordsController extends BaseController {
         for (Records records : list) {
             RecordsVO vo = new RecordsVO();
             vo.setDate(sdf.format(records.getTime()));
-            vo.setAfter(records.getQuantityAfter()/10.0);
-            vo.setBefore(records.getQuantityBefore()/10.0);
-            vo.setQuality(records.getQuantity()/10.0);
+            vo.setAfter(records.getQuantityAfter() / 10.0);
+            vo.setBefore(records.getQuantityBefore() / 10.0);
+            vo.setQuality(records.getQuantity() / 10.0);
             vo.setDocName(records.getDocterName());
             vo.setGentai(records.getGentaiName());
             Integer actionType = records.getActionType();
-            if(actionType == 1) {
+            if (actionType == 1) {
                 vo.setInOutType("出库");
-            }
-            else{
+            } else {
                 vo.setInOutType("入库");
             }
             vo.setTransn(records.getTransactionalNumber());
